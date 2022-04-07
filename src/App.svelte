4034f569fb13d27836a2e725e3203b3c174678bc
Aaron Russell <aaron_russell@intuit.com>
2022-04-06 20:31:20 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	console.log("Hello!")
+</script>
+
+<main>
+	<h1>Hello!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>

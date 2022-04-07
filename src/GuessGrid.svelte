cc62e048941b679c0a013c553270126bd211b865
Aaron Russell <aaron_russell@intuit.com>
2022-04-07 16:23:36 -0600

    Build the guess grid, I guess

---
+++ src/GuessGrid.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let guesses = Array.from({ length: 6 }, () => Array.from({ length: 5 }, () => ""));
+
+    console.log(guesses);
+</script>
+
+<div class="guess-grid">
+    {#each guesses as guess}
+        {#each guess as character}
+            <div class="guess-cell">{character}</div>
+        {/each}
+    {/each}
+</div>
+
+<style>
+    .guess-cell {
+        border: 2px solid var(--color-light-gray);
+        box-sizing: border-box;
+        height: 64px;
+    }
+
+    .guess-grid {
+        display: grid;
+        grid-template-columns: repeat(5, 64px);
+        gap: 8px;
+    }
+</style>

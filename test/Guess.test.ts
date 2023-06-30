84dac7a5f305c62769e2ca657c7191c41d5496dd
Aaron Russell <russell.aaron94@gmail.com>
2023-06-30 09:48:27 -0600

    Migrate to Vite

--- test/Guess.test.ts
+++ test/Guess.test.ts
@@ -1,3 +1,5 @@
+import { describe, expect, it } from "vitest";
+
 import Guess from "../src/Guess";
 import { GuessCharacterColor } from "../src/GuessCharacter";
 
--- test/WordEncoder.test.ts
+++ test/WordEncoder.test.ts
@@ -1,3 +1,5 @@
+import { describe, expect, it } from "vitest";
+
 import dictionary from "../src/dictionary";
 import WordEncoder from "../src/WordEncoder";
 

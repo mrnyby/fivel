ca889c46fb6de47ba84a4422cb2c889719807583
Abi Klein <abi920@gmail.com>
2024-02-05 22:24:55 -0500

    restyle hint

--- src/components/Hint.svelte
+++ src/components/Hint.svelte
@@ -11,14 +11,16 @@
 
 <style>
     div {
-        background: var(--color-translucent-green);
-        border: 1px solid var(--color-green);
+        background: var(--color-light-gray);
+        border: 1px solid var(--color-violet);
         border-radius: 4px;
         box-sizing: border-box;
-        color: var(--color-green);
+        color: var(--color-darker-gray);
         margin-bottom: 8px;
         max-width: 336px;
         padding: 2px 4px;
         text-align: center;
+        font-size: 24px;
+        font-weight: 600;
     }
 </style>
